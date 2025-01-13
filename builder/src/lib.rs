888e06c1f42a77a7a21a225c2d90ad40b1fc694b
Aharon Seidman <aharondov1@gmail.com>
2025-01-12 22:11:00 -0500

    finished beginning of procedural macro practice

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,38 @@
 use proc_macro::TokenStream;
+use quote::quote;
+use syn::{
+    parse_macro_input, punctuated::Punctuated, Data, DataStruct, DeriveInput, Field, Fields,
+    FieldsNamed, Ident, Token,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
+    let ast = parse_macro_input!(input as DeriveInput);
+    let name = &ast.ident;
+    let bname = format!("{}Builder", name);
+    let bident = Ident::new(&bname, name.span());
 
-    unimplemented!()
+    let fields = match &ast.data {
+        Data::Struct(DataStruct { fields, .. }) => match fields {
+            Fields::Named(FieldsNamed { named, .. }) => named.iter(),
+            _ => panic!("expected fields in struct"),
+        },
+        _ => panic!("expected fields in struct"),
+    };
+
+    let mut optionized = Punctuated::new();
+
+    let fields = fields.map(|f| {});
+    let gen = quote! {
+     pub struct #bident {
+            #(#fields,)*
+     }
+        impl #name{
+            fn builder() -> #bident {
+            #bident {
+                }
+            }
+        }
+    };
+    gen.into()
 }
--- main.rs
+++ main.rs
@@ -4,6 +4,18 @@
 //     $ cargo expand
 //
 // To run the code:
-//     $ cargo run
 
-fn main() {}
+use derive_builder::Builder;
+
+#[derive(Builder)]
+pub struct Command {
+    executable: String,
+    args: Vec<String>,
+    env: Vec<String>,
+    current_dir: String,
+}
+
+fn main() {
+    let builder = Command::builder();
+    let _ = builder;
+}
