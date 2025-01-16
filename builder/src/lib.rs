264621f9481377a6bc5ca77d3747bc798b48fe56
Aharon Seidman <aharondov1@gmail.com>
2025-01-15 19:42:59 -0500

    finished test 6

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,6 +1,10 @@
 use proc_macro::TokenStream;
 use quote::quote;
-use syn::{parse_macro_input, Data, DataStruct, DeriveInput, Fields, FieldsNamed, Ident};
+use syn::{
+    parse_macro_input, punctuated::Punctuated, AngleBracketedGenericArguments, Data, DataStruct,
+    DeriveInput, Fields, FieldsNamed, GenericArgument, Ident, Path, PathArguments, PathSegment,
+    Token, Type, TypePath,
+};
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
@@ -19,23 +23,101 @@ pub fn derive(input: TokenStream) -> TokenStream {
     let n_fields = fields.iter().map(|f| {
         let name = &f.ident;
         let ty = &f.ty;
-
-        quote! {
-            #name: Option<#ty>
+        if is_option(&f.ty) {
+            quote! {
+                 #name: #ty
+            }
+        } else {
+            quote! {
+                #name: Option<#ty>
+            }
         }
     });
     let methods = fields.iter().map(|f| {
         let name = &f.ident;
-        let ty = &f.ty;
-        quote! {
-                 fn #name(&mut self, #name: #ty) -> &mut Self {
-                      self.#name = Some(#name);
-                      self
-          }
+        if is_option(&f.ty) {
+            let ty = get_type(&f.ty);
+            quote! {
+                      fn #name(&mut self, #name: #ty) -> &mut Self {
+                           self.#name = Some(#name);
+                           self
+               }
+            }
+        } else {
+            let ty = &f.ty;
+            quote! {
+                     fn #name(&mut self, #name: #ty) -> &mut Self {
+                          self.#name = Some(#name);
+                          self
+              }
+            }
         }
     });
 
-    let iter: Vec<_> = fields.iter().map(|f| &f.ident).collect();
+    let non_option_iter: Vec<_> = fields
+        .iter()
+        .filter(|f| !is_option(&f.ty))
+        .map(|f| &f.ident)
+        .collect();
+    let option_iter: Vec<_> = fields
+        .iter()
+        .filter(|f| is_option(&f.ty))
+        .map(|f| &f.ident)
+        .collect();
+
+    let default_field_values = fields.iter().map(|f| {
+        let field_ident = &f.ident;
+        quote! {
+                #field_ident: None
+        }
+    });
+
+    fn is_option(ty: &Type) -> bool {
+        if let Type::Path(
+            TypePath {
+                path: Path { segments, .. },
+                ..
+            },
+            ..,
+        ) = ty
+        {
+            if let Some(PathSegment { ident, .. }) = segments.first() {
+                if ident.to_string() == "Option" {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    fn get_type(ty: &Type) -> &Punctuated<GenericArgument, Token![,]> {
+        match ty {
+            Type::Path(TypePath {
+                path: Path { segments, .. },
+                ..
+            }) => match &segments[0] {
+                PathSegment {
+                    arguments:
+                        PathArguments::AngleBracketed(AngleBracketedGenericArguments { args, .. }),
+                    ..
+                } => args,
+                _ => panic!("help"),
+            },
+            _ => panic!("help"),
+        }
+    }
+    //Type::Path(
+    //         TypePath {
+    //             qself: None,
+    //             path: Path {
+    //                 segments: [
+    //                     PathSegment {
+    //                         ident: "Option",
 
     let gen = quote! {
      pub struct #bident {
@@ -44,11 +126,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
         impl #name{
             fn builder() -> #bident {
             #bident {
-                 executable: None,
-                 args: None,
-                 env: None,
-                 current_dir: None,
-         }
+                    #(#default_field_values,)*
+                          }
             }
         }
         impl #bident {
@@ -58,7 +137,8 @@ pub fn derive(input: TokenStream) -> TokenStream {
 
     Ok (
         #name {
-            #(#iter: self.#iter.clone().ok_or("missing field")?,)*
+            #(#non_option_iter: self.#non_option_iter.clone().ok_or("missing field")?,)*
+            #(#option_iter: self.#option_iter.clone(),)*
         }
     )
 
--- main.rs
+++ main.rs
@@ -4,7 +4,6 @@
 //     $ cargo expand
 //
 // To run the code:
-
 use derive_builder::Builder;
 
 #[derive(Builder)]
@@ -12,10 +11,24 @@ pub struct Command {
     executable: String,
     args: Vec<String>,
     env: Vec<String>,
-    current_dir: String,
+    current_dir: Option<String>,
 }
 
 fn main() {
-    let builder = Command::builder();
-    let _ = builder;
+    let command = Command::builder()
+        .executable("cargo".to_owned())
+        .args(vec!["build".to_owned(), "--release".to_owned()])
+        .env(vec![])
+        .build()
+        .unwrap();
+    assert!(command.current_dir.is_none());
+
+    let command = Command::builder()
+        .executable("cargo".to_owned())
+        .args(vec!["build".to_owned(), "--release".to_owned()])
+        .env(vec![])
+        .current_dir("..".to_owned())
+        .build()
+        .unwrap();
+    assert!(command.current_dir.is_some());
 }
